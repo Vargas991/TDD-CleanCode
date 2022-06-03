0271fee6d10a2786ab478809d46b92bafcb72736
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-03 11:52:02 -0500

    refactor: foodHelper injected on constructor

--- src/_tests_/food.spec.ts
+++ src/_tests_/food.spec.ts
@@ -2,7 +2,11 @@ import { Food } from "../models/food-model"
 
 describe('Classes Food ',()=>{
     test('food class',()=>{
-        const food = new Food('Pollo','Pollo frito',30)
+        const food = new Food({
+                name: 'Pollo',
+                description: 'Pollo frito', 
+                price:30
+            })
 
         expect(food.getName()).toEqual('Pollo')
         expect(food.getDescription()).toEqual('Pollo frito')
