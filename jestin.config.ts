bb5cd0f6b0c38d1bd1e0209e465a18246564d171
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-02 22:59:19 -0500

    refactor: add function for each property VechicleModel

---
+++ jestin.config.ts
@@ -0,0 +1,30 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+// import type {Config} from '@jest/types';
+
+// // Sync object
+// const config: Config.InitialOptions = {
+//   verbose: true,
+// };
+// export default config;
+
+// Or async function
+// export default async (): Promise<Config.InitialOptions> => {
+//   return {
+//     verbose: true,
+//   };
+// };
+// export default {
+//   roots: ['<rootDir>/src'],
+//   collectCoverageFrom: ['<rootDir>/src/***/*.ts'],
+//   collectCoverage: true,
+//   coverageDirectory: 'coverage',
+//   coverageProvider: 'v8',
+//   testEnvironment: 'node',
+//   transform: {
+//     '.+\\.ts$': 'ts-jest'
+//   }
+// };
