43f3772f4215ada71505189fbede114d2cd92c58
vargas991 <edderson.vargas@unet.edu.ve>
2022-06-09 01:28:45 -0500

    refactor: add invalidArgument in UrlLogin

--- src/protocols/protocols-http.ts
+++ src/protocols/protocols-http.ts
@@ -1,7 +1,11 @@
 import { UrlWithParsedQuery, parse } from "url";
+import { InvalidArgument } from "../errors/invalid-arg";
 
 export class UrlLogin {
     public static parseUrl (url: string): UrlWithParsedQuery {  
+        if(!url){
+            throw new InvalidArgument(url)
+        }
         return parse(url, true)
     }
 }
